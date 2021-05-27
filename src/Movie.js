d674be6bdee07c7df7a8f5baa642928456948e0b
clap1030 <clap1030@gmail.com>
2021-05-27 23:37:08 +0900

    TEST COmMIT

--- src/Movie.js
+++ src/Movie.js
@@ -2,7 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 
 
-function Movie({id, year, title, summary, poster}){
+function Movie({key, id, year, title, summary, poster}){
     return <h4>{id}, {year}, {title}, {summary}, {poster}</h4>;
 }
 
